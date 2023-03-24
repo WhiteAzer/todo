de22e64a53ef592dafc2ce5057aa5d5e9d915848
WhiteAzer <0202timur2020@gmail.com>
2023-03-24 19:23:25 +0300

    feat: +MoreBtn

---
+++ src/features/MoreBtn/MoreBtn.tsx
@@ -0,0 +1,13 @@
+import styles from './MoreBtn.module.scss';
+import MoreBtnIcon from '../../assets/more-btn.svg';
+import { BtnThemes, Button } from '../../components/Button/Button';
+import { PropSize } from '../../types/global';
+import { FC, HTMLAttributes } from 'react';
+
+export const MoreBtn: FC<HTMLAttributes<HTMLButtonElement>> = ({ ...props }) => {
+	return (
+		<Button theme={BtnThemes.NONE} size={PropSize.XS} className={styles.btn} {...props}>
+			<MoreBtnIcon />
+		</Button>
+	);
+};
