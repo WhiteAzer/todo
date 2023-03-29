9598df2b24340870f770ffa04c662977b85967ac
WhiteAzer <0202timur2020@gmail.com>
2023-03-29 20:10:50 +0300

    fix: fixed Checkbox

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -3,7 +3,6 @@ import styles from './Checkbox.module.scss';
 import classNames from 'classnames';
 import { TPropsWithClass } from '../../types/global';
 import CheckboxIcon from '../../assets/checkbox.svg';
-import { useToggle } from '../../hooks/useToggle';
 
 type TProps = TPropsWithClass<{
 	label?: string;
@@ -11,11 +10,9 @@ type TProps = TPropsWithClass<{
 }>;
 
 export const Checkbox: FC<TProps> = ({ label, isChecked, className }) => {
-	const [check, toggleCheck] = useToggle(isChecked);
-
 	return (
-		<div className={classNames(styles.wrapper, className)} onClick={toggleCheck}>
-			<div className={styles.checkbox}>{check && <CheckboxIcon />}</div>
+		<div className={classNames(styles.wrapper, className)}>
+			<div className={styles.checkbox}>{/*check*/ isChecked && <CheckboxIcon />}</div>
 			<span className={styles.label}>{label}</span>
 		</div>
 	);
