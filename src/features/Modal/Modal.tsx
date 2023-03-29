926a5c81d12d3604cd54e8f48b399e6271e61ec0
WhiteAzer <0202timur2020@gmail.com>
2023-03-29 20:15:14 +0300

    fix: fixed Modal

--- src/features/Modal/Modal.tsx
+++ src/features/Modal/Modal.tsx
@@ -19,24 +19,20 @@ export const Modal: FC<TProps> = ({ isOpen, closeModal, children }) => {
 	);
 
 	useEffect(() => {
-		console.log('mount');
 		isOpen
-			? document.addEventListener('keydown', handleEnter)
-			: document.removeEventListener('keydown', handleEnter);
-		return () => console.log('unmount');
+			? window.addEventListener('keydown', handleEnter)
+			: window.removeEventListener('keydown', handleEnter);
 	}, [handleEnter, isOpen]);
 
-	const stopPropagation: MouseEventHandler<HTMLDivElement> = e => {
-		e.stopPropagation();
+	const handleClose: MouseEventHandler<HTMLDivElement> = e => {
+		if (e.target === e.currentTarget) closeModal();
 	};
 
 	if (isOpen) {
 		return (
 			<Portal>
-				<div className={styles.wrapper} onClick={closeModal}>
-					<div className={styles.content} onClick={stopPropagation}>
-						{children}
-					</div>
+				<div className={styles.wrapper} onClick={handleClose}>
+					<div className={styles.content}>{children}</div>
 				</div>
 			</Portal>
 		);
