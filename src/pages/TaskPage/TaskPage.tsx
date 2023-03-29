708f7b2716868dace32c72fbc743256aeed259a2
WhiteAzer <0202timur2020@gmail.com>
2023-03-29 20:11:44 +0300

    feat: +TaskPage

---
+++ src/pages/TaskPage/TaskPage.tsx
@@ -0,0 +1,17 @@
+import styles from './TaskPage.module.scss';
+import { BtnThemes, Button } from '../../components/Button/Button';
+import { PropSize } from '../../types/global';
+import BackBtn from './../../assets/back-btn.svg';
+import { TaskForm } from '../../features/TaskForm/TaskForm';
+
+export const TaskPage = () => {
+	return (
+		<main className={styles.wrapper}>
+			<Button theme={BtnThemes.NONE} size={PropSize.XS} className={styles.backBtn}>
+				<BackBtn />
+				Вернуться к задачам
+			</Button>
+			<TaskForm label={'Todo'} btnSize={PropSize.S}></TaskForm>
+		</main>
+	);
+};
