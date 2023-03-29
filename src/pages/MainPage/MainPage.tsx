0285b3fbaca3826b8f0a6470a97c605c36503db3
WhiteAzer <0202timur2020@gmail.com>
2023-03-29 20:17:51 +0300

    feat: +MainPage

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,16 +1,19 @@
 import { Filters } from '../../features/Filters/Filters';
 import styles from './MainPage.module.scss';
 import { TasksColumn } from '../../features/TasksColumn/TasksColumn';
+import { TaskColumns } from '../../types/data';
 
-export const MainPage = () => {
+const MainPage = () => {
 	return (
 		<main className={styles.wrapper}>
 			<Filters className={styles.filters} />
 			<div className={styles.columns}>
-				<TasksColumn title={'Todo'} />
-				<TasksColumn title={'In progress'} />
-				<TasksColumn title={'Done'} />
+				<TasksColumn type={TaskColumns.TODO} />
+				<TasksColumn type={TaskColumns.IN_PROGRESS} />
+				<TasksColumn type={TaskColumns.DONE} />
 			</div>
 		</main>
 	);
 };
+
+export default MainPage;
