9bc14eeec6c47186866ffbe2dbe5b0490113a7bc
WhiteAzer <0202timur2020@gmail.com>
2023-04-04 18:31:32 +0300

    feat: +TaskPage

--- src/pages/TaskPage/TaskPage.tsx
+++ src/pages/TaskPage/TaskPage.tsx
@@ -1,17 +1,46 @@
 import styles from './TaskPage.module.scss';
-import { BtnThemes, Button } from '../../components/Button/Button';
-import { PropSize } from '../../types/components';
 import BackBtn from './../../assets/back-btn.svg';
-import { TaskForm } from '../../features/TaskForm/TaskForm';
+import { Link } from 'react-router-dom';
+import { TaskFormEdit } from '../../features/Common/TaskFormEdit/TaskFormEdit';
+import { MoreBtn } from '../../features/Common/MoreBtn/MoreBtn';
+import { TaskPopup } from '../../features/TaskPage/TaskPopup/TaskPopup';
+import { useToggle } from '../../hooks/useToggle';
+import { PropSize } from '../../types/components';
+import { MouseEventHandler, useCallback, useState } from 'react';
+import { AddCommentBtn } from '../../features/TaskPage/AddCommentBtn/AddCommentBtn';
 
 export const TaskPage = () => {
+	const [isPopupVisible, togglePopupVisible] = useToggle(false);
+	const [canEdit, setCanEdit] = useState(false);
+
+	const handlePopupClick = useCallback<MouseEventHandler<HTMLButtonElement>>(
+		e => {
+			e.stopPropagation();
+			togglePopupVisible();
+		},
+		[togglePopupVisible]
+	);
+
+	const handleEdit = useCallback(() => {
+		setCanEdit(true);
+	}, []);
+
 	return (
 		<main className={styles.wrapper}>
-			<Button theme={BtnThemes.NONE} size={PropSize.XS} className={styles.backBtn}>
+			<Link to={'../'} className={styles.backLink}>
 				<BackBtn />
 				Вернуться к задачам
-			</Button>
-			{/*<TaskForm label={'Todo'} btnSize={PropSize.S}></TaskForm>*/}
+			</Link>
+			<TaskFormEdit isFull={true} btnSize={PropSize.S} canEdit={canEdit}>
+				<MoreBtn className={styles.moreBtn} onClick={handlePopupClick} type={'button'} />
+				<TaskPopup
+					isVisible={isPopupVisible}
+					handleClose={togglePopupVisible}
+					className={styles.popup}
+					handleEdit={handleEdit}
+				/>
+				{canEdit && <AddCommentBtn />}
+			</TaskFormEdit>
 		</main>
 	);
 };
